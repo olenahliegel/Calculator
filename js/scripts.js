e8ae90332d0809a1d4e41724494a5eae296c6d3f
Olena Shliegel <olenashliegel@gmail.com>
2023-10-26 10:55:10 -0700

    It will add the business logic

--- js/scripts.js
+++ js/scripts.js
@@ -3,8 +3,24 @@ function add(number1, number2) {
   return number1 + number2;
 }
 
+function subtract(number1, number2) {
+  return number1 - number2;
+}
+
+function multiply(number1, number2) {
+  return number1 * number2;
+}
+
+function divide(number1, number2) {
+  return number1 / number2;
+}
+
 // user interface logic 
 const number1 = parseInt(prompt("Enter a number:"));
 const number2 = parseInt(prompt("Enter another number:"));
 
-window.alert(add(number1, number2));
+window.alert('The result of function add: ' + add(number1, number2) + '.' +
+  ' The result of function subtract: ' + subtract(number1, number2) + '.' +
+  ' The result of function multiply: ' + multiply(number1, number2) + '.' +
+  ' The result of function divide: ' + divide(number1, number2) + '.'
+);
